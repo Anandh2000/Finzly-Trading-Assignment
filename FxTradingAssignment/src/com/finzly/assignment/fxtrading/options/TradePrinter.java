00dda196bdd49b1be53964509386aeab051fca4c
Anandh2000 <110973366+Anandh2000@users.noreply.github.com>
2022-09-03 16:28:15 +0530

    Add files via upload

--- FxTradingAssignment/src/com/finzly/assignment/fxtrading/options/TradePrinter.java
+++ FxTradingAssignment/src/com/finzly/assignment/fxtrading/options/TradePrinter.java
@@ -8,13 +8,17 @@ public class TradePrinter {
 	
 	//prints the data stored in linkedhashset and the timecomplexity is O(1) 
 	public void printData(LinkedHashSet<FxDataManager> tradeData) {
-	
-		System.out.println("***********************************TRADING DATA*************************************");
-		System.out.printf("%s %14s %18s %15s %17s\n","TradeNo","CurrencyPair","CustomerName","Amount","Rate");
-		for(FxDataManager fxData : tradeData) {
-			System.out.printf("%3s %13s %21s %20s %14s",fxData.getTradeNumber(),fxData.getCurrencyPair(),
-					fxData.getCustomerName(),new FxDataManager().formatedAmount(fxData.getAmount()),fxData.getRate());
-			System.out.println();
+		if(tradeData.isEmpty()) {
+			System.out.println("There is No data to show");
+		}
+		else {
+			System.out.println("***********************************TRADING DATA*************************************");
+			System.out.printf("%s %14s %18s %15s %17s\n","TradeNo","CurrencyPair","CustomerName","Amount","Rate");
+			for(FxDataManager fxData : tradeData) {
+				System.out.printf("%3s %13s %21s %20s %14s",fxData.getTradeNumber(),fxData.getCurrencyPair(),
+						fxData.getCustomerName(),new FxDataManager().formatedAmount(fxData.getAmount()),fxData.getRate());
+				System.out.println();
+			}
 		}
 		
 		
